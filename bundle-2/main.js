1aa5e545a724d6c9a6df5bbcf21e660454d71130
cioby09 <claudiociobanu09@gmail.com>
2023-03-27 17:54:41 +0200

    Bundle 2 - Esercizio 4

--- bundle-2/main.js
+++ bundle-2/main.js
@@ -11,20 +11,20 @@
 
 
 // ESERCIZIO 1
-// for (let i = 0; i < 5; i++) { // Dato che i parte da 0, la condizione i > 5 non si avvera perciò sostituisco con la condizione i < 5
-//     console.log(i);
-// }
+for (let i = 0; i < 5; i++) { // Dato che i parte da 0, la condizione i > 5 non si avvera perciò sostituisco con la condizione i < 5
+    console.log(i);
+}
 
 
 // ESERCIZIO 2
-// function addIfEven(num) {
-//     if (num % 2 === 0) { //Affinchè mi ritorni un valore numerico metto tre uguali
-//         return num + 5;
-//     }
-//     return num;
-// }
-// // Stampo su console per avere un output
-// console.log(addIfEven(6));
+function addIfEven(num) {
+    if (num % 2 === 0) { //Affinchè mi ritorni un valore numerico metto tre uguali
+        return num + 5;
+    }
+    return num;
+}
+// Stampo su console per avere un output
+console.log(addIfEven(6));
 
 // ESERCIZIO 3
 function loopToFive() {
@@ -37,14 +37,15 @@ loopToFive();
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 8 errori)
-// function displayEvenNumbers() {
-//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-//     let evenNumbers = [];
-//     for (let i = 0; i < numbers.length - 1; i++;) {
-//         if (numbers % 2 = 0); {
-//             evenNumbers.push(i);
-//         }
-//         return evenNumbers;
-//     }
-// }
-// displayEvenNumbers(); // [2,4,6,8]
+function displayEvenNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = [];
+    for (let i = 0; i < numbers.length; i++) { // Tolgo il punto e virgola dopo "i++"(Errore 1) e tolgo la sottrazione di un 1 alla condizione(Errore 2)
+        if (numbers[i] % 2 === 0) { // Tolgo il punto e virgola dopo la condizione (Errore 3), sostituisco l'uguale con il triplo uguale(Errore 4) e aggiungo "[i]" a numbers affinchè selezioni un elemento dell'array(Errore 5)
+            evenNumbers.push(numbers[i]); //Correggo il valore di [i] con il valore [i] di numbers(Errore 6)
+        }
+    }
+    return evenNumbers; // Sposto return affinchè non interrompa la funzione(Errore 7)
+}
+// Invoco la funzione all'interno di un console log per stampare su console(Errore 8)
+console.log(displayEvenNumbers());; // [2,4,6,8]
