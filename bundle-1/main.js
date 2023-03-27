6920dcc592b766599ce1d536d2c5f5340f9a185c
cioby09 <claudiociobanu09@gmail.com>
2023-03-27 17:34:02 +0200

    Bundle 2 - Esercizio 1

--- bundle-1/main.js
+++ bundle-1/main.js
@@ -11,42 +11,42 @@
 
 
 // ESERCIZIO 1
-// function checkAge() {
-//     const myAge = 32;
-//     // const message = ''; 
-//     // Dato che abbiamo bisogno di dar valori differenti a "message" non possiamo renderlo una costante
-//     let message = ''; // con let abbiamo una variabile assegnata a "message"
+function checkAge() {
+    const myAge = 32;
+    // const message = ''; 
+    // Dato che abbiamo bisogno di dar valori differenti a "message" non possiamo renderlo una costante
+    let message = ''; // con let abbiamo una variabile assegnata a "message"
 
-//     if (myAge < 18) {
-//         message = `Sei troppo giovane! Hai ${myAge} anni!`;
-//     } else {
-//         message = 'Hai più di 18 anni!';
-//     }
-//     return message;
-//     // Termino la funzione e restituisco "message"
-// }
-// console.log(checkAge());
-// // Stampo su console
+    if (myAge < 18) {
+        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+    } else {
+        message = 'Hai più di 18 anni!';
+    }
+    return message;
+    // Termino la funzione e restituisco "message"
+}
+console.log(checkAge());
+// Stampo su console
 
 
 // ESERCIZIO 2
-// function printColorsNumber() {
-//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-//     // console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
-//     // LENGTH non è scritto correttamente
-//     console.log(`Nella mia palette ci sono ${colors.length} colori!`);
-// }
-// printColorsNumber();
+function printColorsNumber() {
+    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+    // console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    // LENGTH non è scritto correttamente
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
+}
+printColorsNumber();
 
 
 // ESERCIZIO 3
-// function addNumbers() {
-//     const userNumber = parseInt(prompt('Inserisci un numero'));
-//     // Aggiungo parseint per convertire le stringhe in numeri interi
-//     const total = userNumber + 1;
-//     console.log(`Il risultato finale è ${total}`);
-// }
-// addNumbers();
+function addNumbers() {
+    const userNumber = parseInt(prompt('Inserisci un numero'));
+    // Aggiungo parseint per convertire le stringhe in numeri interi
+    const total = userNumber + 1;
+    console.log(`Il risultato finale è ${total}`);
+}
+addNumbers();
 
 
 // ESERCIZIO 4
